090446354d6968fd3c61ad9fbf79f966e043eddd
Venky Venkataraman <venky76v@gmail.com>
2017-08-07 21:34:14 +0100

    Added Feature 1

---
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    public class Feature1
+    {
+        public string FirstName { get; set; } = "Venky";
+        public string LastName { get; set; } = "Venkataraman";
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,12 @@ namespace ConsoleApp1
             // code was changed in github
             Console.WriteLine("Some code changed in github as a part of Git Fundamentals channel 9 msdn tutorial");
             Console.Read();
+            // code to call feature 1
+            var person = new Feature1();
+            Console.WriteLine(person.FullName);
+            person.LastName = "Skeet";
+            person.FirstName = "Jon";
+            Console.WriteLine(person.FullName);
         }
     }
 }
